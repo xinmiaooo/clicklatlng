e08e3212977f552b26404a67e2f6c7a2d77acf81
Qu Xinmiao <83005951+xinmiaooo@users.noreply.github.com>
2026-03-30 17:33:35 +0900

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,22 @@
 import adapter from '@sveltejs/adapter-static';
-import { relative, sep } from 'node:path';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		// defaults to rune mode for the project, execept for `node_modules`. Can be removed in svelte 6.
-		runes: ({ filename }) => {
-			const relativePath = relative(import.meta.dirname, filename);
-			const pathSegments = relativePath.toLowerCase().split(sep);
-			const isExternalLibrary = pathSegments.includes('node_modules');
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-			return isExternalLibrary ? undefined : true;
-		}
-	},
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({			
+		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html',
+			fallback: undefined,
 			precompress: false,
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.BASE_PATH || ''
 		}
 	}
 };
