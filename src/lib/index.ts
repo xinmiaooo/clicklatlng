8eeae4cf6370d7c0e1c3b985ad82ae112b792c50
Qu Xinmiao <83005951+xinmiaooo@users.noreply.github.com>
2026-03-30 17:47:44 +0900

    fade out

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 export async function getLngLat(event: maplibregl.MapMouseEvent): Promise<string> {
-    const lngLat = `${event.lngLat.lng}, ${event.lngLat.lat}`;
+    const lngLat = `${event.lngLat.lng.toFixed(6)}, ${event.lngLat.lat.toFixed(6)}`;
     await navigator.clipboard.writeText(lngLat);
     return lngLat;
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,33 @@
 <script lang="ts">
-	import { MapLibre } from 'svelte-maplibre-gl';
-	import { getLngLat } from '$lib';
+  import { MapLibre } from "svelte-maplibre-gl";
+  import { getLngLat } from "$lib";
 
-	let copyLngLat: string = $state('');
+  let copyLngLat: string = $state("");
+  let visible: boolean = $state(false);
+
+  function showToast(text: string) {
+    copyLngLat = text;
+    visible = true;
+    setTimeout(() => (visible = false), 2000);
+    setTimeout(() => (copyLngLat = ""), 2500);
+  }
 </script>
 
 <MapLibre
-	onclick={async (event) => {
-		copyLngLat = await getLngLat(event);
-		setTimeout(() => (copyLngLat = ''), 3000);
-	}}
-	class="h-screen w-screen"
-	style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+  onclick={async (event) => {
+    showToast(await getLngLat(event));
+  }}
+  class="h-screen w-screen"
+  style="https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+  center={[142.78848876304016, 43.3316837806498]}
+  zoom={6}
 />
 
-{#if copyLngLat}
-	<div class="fixed top-4 left-1/2 -translate-x-1/2 bg-black/75 text-white px-4 py-2 rounded shadow">
-		copied {copyLngLat}
-	</div>
-{/if}
+<div
+  class="fixed top-4 left-1/2 -translate-x-1/2 bg-white/75 text-black px-9 py-5 rounded-xl shadow
+    transition-[opacity,display] duration-500 transition-discrete starting:opacity-0"
+  class:hidden={!copyLngLat}
+  class:opacity-0={!visible}
+>
+  copied {copyLngLat}
+</div>
